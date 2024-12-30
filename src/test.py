e4457a75667f4b6aa36b7cd5abede29f9e6e2c73
OsawaKousei <jwithelis@gmail.com>
2024-12-31 00:26:38 +0900

    pass test

--- src/test.py
+++ src/test.py
@@ -1,22 +1,34 @@
-import os
-
 import torch
-from diffusers import StableDiffusion3Pipeline
+from diffusers import (
+    BitsAndBytesConfig,
+    SD3Transformer2DModel,
+    StableDiffusion3Pipeline,
+)
 
-token = os.getenv("HF_TOKEN")
-print(token)
+model_id = "stabilityai/stable-diffusion-3.5-medium"
 
-pipe = StableDiffusion3Pipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3.5-medium",
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+)
+model_nf4 = SD3Transformer2DModel.from_pretrained(
+    model_id,
+    subfolder="transformer",
+    quantization_config=nf4_config,
     torch_dtype=torch.bfloat16,
     cache_dir="./models",
-    use_auth_token=token,
 )
-pipe = pipe.to("cuda")
 
-image = pipe(
-    "A capybara holding a sign that reads Hello World",
+pipeline = StableDiffusion3Pipeline.from_pretrained(
+    model_id, transformer=model_nf4, torch_dtype=torch.bfloat16, cache_dir="./models"
+)
+pipeline.enable_model_cpu_offload()
+
+prompt = "A capybara holding a sign that reads Hello World"
+
+image = pipeline(
+    prompt=prompt,
     num_inference_steps=40,
     guidance_scale=4.5,
+    max_sequence_length=512,
 ).images[0]
-image.save("capybara.png")
+image.save("./result/output.png")

726022078ce077343faf7f48642d68bcb01234e3
OsawaKousei <jwithelis@gmail.com>
2024-12-30 12:10:52 +0900

    transfer to devcontainer

--- src/huggingface.py
+++ src/huggingface.py
@@ -11,25 +11,31 @@ from pathlib import Path
 import torch
 from diffusers import StableDiffusionPipeline
 
-hub_dir = Path(os.getenv("HOME"))/".cache/huggingface/hub"
+hub_dir = Path(os.getenv("HOME")) / ".cache/huggingface/hub"
 model = str("/home/kousei/.cache/huggingface/hub/model/Brav6.safetensors")
-pipe = StableDiffusionPipeline.from_single_file(
-    model,
-    torch_dtype=torch.float16
-).to("cuda")
+pipe = StableDiffusionPipeline.from_single_file(model, torch_dtype=torch.float16).to(
+    "cuda"
+)
 
 # EasyNegativeV2 を使いたい場合
 pipe.load_textual_inversion(
     "gsdf/Counterfeit-V3.0",
     weight_name="embedding/EasyNegativeV2.safetensors",
-    token="EasyNegativeV2"
+    token="EasyNegativeV2",
 )
 
-prompt = "woman,70 years old, high resolution, realistic, portrait, japanese, grey hair, smile, traditional, beautiful skin, fat, kimono"
-negative_prompt = "EasyNegative, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), split view, grid view, monochrome,(wrinkle:3)"
-#negative_prompt="EasyNegativeV2, extra fingers, fewer fingers"
+prompt = (
+    "woman,70 years old, high resolution, realistic, portrait, japanese, grey hair, smile, "
+    "traditional, beautiful skin, fat, kimono"
+)
+negative_prompt = (
+    "EasyNegative, paintings, sketches, (worst quality:2), (low quality:2), "
+    "(normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), "
+    "split view, grid view, monochrome,(wrinkle:3)"
+)
+# negative_prompt="EasyNegativeV2, extra fingers, fewer fingers"
 
-#generator = None
+# generator = None
 # seed を固定したい場合
 generator = torch.Generator()
 generator.manual_seed(1234)
